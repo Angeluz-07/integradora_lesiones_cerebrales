b83395e06bdd123893ef736dc09f05190334bca0
admacas <admacas@espol.edu.ec>
2021-01-10 10:58:22 -0500

    cambios realizados

--- prototipo_brain_lesion/diagnostico/urls.py
+++ prototipo_brain_lesion/diagnostico/urls.py
@@ -3,7 +3,7 @@ from diagnostico.views import diagnostic, serve_file, save_diagnostic, ListarDia
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import TemplateView 
 urlpatterns = [  
-    
+    path('home/',TemplateView.as_view(template_name='home.html'), name="home"),
     path('diagnostic/<str:type_>',diagnostic, name='diagnostic'),
     path('save/',save_diagnostic,name='save_diagnostic'),
     path('listar/',ListarDiagnostico.as_view(),name='listDiagnostic'),
