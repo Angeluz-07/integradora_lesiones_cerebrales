b7bf81a9e06a4079670e1336a9e3ff5c09a47dd8
admacas <admacas@espol.edu.ec>
2021-01-09 23:31:49 -0500

    create branch

--- prototipo_brain_lesion/prototipo_brain_lesion/urls.py
+++ prototipo_brain_lesion/prototipo_brain_lesion/urls.py
@@ -19,7 +19,8 @@ from django.urls import path, include
  
 
 urlpatterns = [
-    path('',include('diagnostico.urls')),
+    path('',TemplateView.as_view(template_name='home.html'), name="home"),
+    path('diagnostico',include('diagnostico.urls')),
     path('login/',LoginView.as_view(template_name='login.html'),name="login"),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('admin/', admin.site.urls),
